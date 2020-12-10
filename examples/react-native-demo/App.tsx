7267c0e0c6711409e2f00a1c1ec77b0fe2173a44
Brian Li <brian.li.101@gmail.com>
2020-12-10 16:01:00 -0500

    some native files for View testing

--- examples/react-native-demo/App.tsx
+++ examples/react-native-demo/App.tsx
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+
+import { View } from '../../react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView>
+			<View style={{ height: 100, width: 100, backgroundColor: 'red' }}>
+				<Text>Open up App.tsx to start working on your app!</Text>
+				<StatusBar style="auto" />
+			</View>
+		</SafeAreaView>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
