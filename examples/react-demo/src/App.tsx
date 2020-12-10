900ecb1ab966bd625e15f232c48c30bc04866608
Brian Li <brian.li.101@gmail.com>
2020-12-10 16:17:48 -0500

    using cyclops as a local package for npm on web

--- examples/react-demo/src/App.tsx
+++ examples/react-demo/src/App.tsx
@@ -2,25 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//import { View } from '../../../react-native';
+import { View } from 'cyclops/react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>hello</h1>
+			<View
+				style={{ height: 100, width: 100, backgroundColor: 'red' }}
+			></View>
+		</div>
+	);
 }
 
 export default App;
