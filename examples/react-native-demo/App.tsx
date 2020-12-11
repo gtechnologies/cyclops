b86602dde168c7c0f7c66d7ce2e6eebf3179217b
Brian Li <brian.li.101@gmail.com>
2020-12-11 16:52:50 -0500

    new react-native demo app, import issues exist

--- examples/react-native-demo/App.tsx
+++ examples/react-native-demo/App.tsx
@@ -1,16 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import {SafeAreaView, StyleSheet, StatusBar} from 'react-native';
 
-import { View } from '../../react-native';
+import {View} from 'cyclops/native';
 
-export default function App() {
-	return (
-		<SafeAreaView>
-			<View style={{ height: 100, width: 100, backgroundColor: 'red' }}>
-				<Text>Open up App.tsx to start working on your app!</Text>
-				<StatusBar style="auto" />
-			</View>
-		</SafeAreaView>
-	);
-}
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View style={{width: 100, height: 100, backgroundColor: 'red'}}>
+          <View style={{width: 20, height: 20, backgroundColor: 'pink'}}></View>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
