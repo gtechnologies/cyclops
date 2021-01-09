422f010ecf7062ebc61df2198d39a966806af2a6
Brian Li <brian.li.101@gmail.com>
2021-01-08 21:47:07 -0500

    styling for rounded TextInput on mobile

--- src/components/TextInput/TextInput.native.tsx
+++ src/components/TextInput/TextInput.native.tsx
@@ -1,6 +1,36 @@
 import React from 'react';
-import { TextInput as RNTextInput, TextInputProps } from 'react-native';
+import { TextInput as RNTextInput, ViewStyle } from 'react-native';
+
+import { Colors } from '../../styles/Colors';
+
+import {
+	TextInputPropsNative as TextInputProps,
+	TextType,
+	TextTypeEnum,
+	TextInputType,
+	TextInputTypeEnum,
+} from './TextInputTypes';
+
+const textInputStyleDictionary: { [type: string]: ViewStyle } = {
+	[TextInputTypeEnum.none]: {},
+	[TextInputTypeEnum.rounded]: {
+		borderRadius: 20,
+		padding: 10,
+		backgroundColor: Colors.LIGHT_GRAY,
+	},
+};
 
 export const TextInput = (props: TextInputProps) => {
-	return <RNTextInput {...props} />;
+	// TODO: fix how style is implemented here and add it to web as well but this is temporary to be a non blocker
+	let textInputType = props.type ? props.type : 'none';
+	let style: ViewStyle = {
+		...textInputStyleDictionary[textInputType],
+		...(props.style as ViewStyle),
+	};
+
+	return <RNTextInput {...props} style={style} />;
+};
+
+TextInput.defaultProps = {
+	type: 'none',
 };
