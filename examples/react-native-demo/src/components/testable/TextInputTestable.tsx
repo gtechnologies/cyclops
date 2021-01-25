bc2a1313a1e8d16e3c67b73e042ea557acb89ba0
Brian Li <brian.li.101@gmail.com>
2021-01-24 23:01:20 -0500

    fixing ref forwarding for View elements and TextInput

--- examples/react-native-demo/src/components/testable/TextInputTestable.tsx
+++ examples/react-native-demo/src/components/testable/TextInputTestable.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import { TextInput } from 'cyclops/native';
+import { TextInput as RNTextInput } from 'react-native';
+import { TextInput, VFlex } from 'cyclops/native';
 
 export class TextInputTestable extends React.Component {
+	rnTextInputRef = React.createRef<RNTextInput>();
+	cyclopsTextInputRef = React.createRef<TextInput>();
+
 	render() {
 		return (
-			<TextInput
-				style={{
-					width: '100%',
-					marginTop: 20,
-				}}
-				placeholder="Hallo"
-				//multiline={true}
-				type="rounded"
-			/>
+			<VFlex>
+				<VFlex style={{ height: 20 }} />
+				<RNTextInput
+					ref={this.rnTextInputRef}
+					autoFocus={true}
+					onEndEditing={() =>
+						this.cyclopsTextInputRef.current?.focus()
+					}
+					style={{ backgroundColor: 'red' }}
+				/>
+				<TextInput
+					ref={this.cyclopsTextInputRef}
+					style={{
+						width: '100%',
+						marginTop: 20,
+					}}
+					placeholder="Hallo"
+					//multiline={true}
+					type="rounded"
+				/>
+			</VFlex>
 		);
 	}
 }
