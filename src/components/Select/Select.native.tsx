b6afc2dbf866dc156f60dd068b1eac86b00b0813
AustinCarter <austin@aktri.com>
2021-03-11 18:17:29 -0500

    update native select

--- src/components/Select/Select.native.tsx
+++ src/components/Select/Select.native.tsx
@@ -1,8 +1,8 @@
 import React, { ReactNode } from 'react';
 import { useState } from 'react';
 import {
-	TextStyle as RNTextStyle,
-	ViewStyle as RNViewStyle,
+	TextStyle as RNWTextStyle,
+	ViewStyle as RNWViewStyle,
 } from 'react-native';
 
 import { CheckBox } from '../CheckBox/CheckBox.native';
@@ -16,8 +16,10 @@ interface Props {
 	options: string[];
 	multiSelect?: boolean;
 	onChange?: (value: string | string[]) => void;
-	labelStyle?: RNTextStyle;
-	style?: RNViewStyle;
+	labelStyle?: RNWTextStyle;
+	style?: RNWViewStyle;
+	color?: string;
+	size?: number;
 	defaultSelection?: string[];
 }
 
@@ -25,7 +27,7 @@ const defaultLabelStyle = {
 	paddingLeft: Padding.ELEMENT_WEB,
 };
 
-export function Select(props: Props) {
+export const Select: React.FunctionComponent<Props> = (props: Props) => {
 	var optionElemets: ReactNode = [];
 
 	let labelStyle = {
@@ -33,11 +35,12 @@ export function Select(props: Props) {
 		...props.labelStyle,
 	};
 
+	const [selection, setSelection] = useState<string[] | undefined>(
+		props.defaultSelection,
+	);
+
 	if (props.multiSelect) {
 		// TODO: find a way to make useState type to string array that doesn't require filtering out the empty string later
-		const [selection, setSelection] = useState(
-			props.defaultSelection ?? [''],
-		);
 		optionElemets = props.options.map((option) => {
 			return (
 				<View style={{ padding: Padding.ELEMENT_WEB }}>
@@ -45,34 +48,30 @@ export function Select(props: Props) {
 						onPress={(e: any) => {
 							//need local var so it is updated immediately
 							var res = selection;
-							if (res.indexOf(option) >= 0)
-								res = selection.filter(
+							if (res && res.indexOf(option) >= 0)
+								res = selection?.filter(
 									(item) => item != option && item != '',
 								);
-							else res = [...selection, option];
+							else
+								res = selection
+									? [...selection, option]
+									: [option];
 
 							setSelection(res);
-							if (props.onChange) props.onChange(res);
+							if (props.onChange) props.onChange(res ?? []);
 						}}
 						style={{ display: 'flex', flexDirection: 'row' }}
+						alignItemsV="center"
 					>
 						<CheckBox
-							color={Colors.BASE}
+							style={props.style}
+							color={props.color ?? Colors.BASE}
 							value={
-								selection.indexOf(option) >= 0 ? true : false
+								selection && selection.indexOf(option) >= 0
+									? true
+									: false
 							}
-							onValueChange={() => {
-								//need local var so it is updated immediately
-								var res = selection;
-								if (res.indexOf(option) >= 0)
-									res = selection.filter(
-										(item) => item != option && item != '',
-									);
-								else res = [...selection, option];
-
-								setSelection(res);
-								if (props.onChange) props.onChange(res);
-							}}
+							size={props.size}
 						/>
 						<Text style={labelStyle} type="body">
 							{option}
@@ -82,26 +81,30 @@ export function Select(props: Props) {
 			);
 		});
 	} else {
-		const [selection, setSelection] = useState(
-			props.defaultSelection ? props.defaultSelection[0] : '',
-		);
 		optionElemets = props.options.map((option) => {
 			return (
 				<View style={{ padding: Padding.ELEMENT_WEB }}>
 					<TouchableOpacity
 						onPress={(e: any) => {
-							setSelection(option);
+							setSelection([option]);
 							if (props.onChange) props.onChange(option);
 						}}
 						style={{ display: 'flex', flexDirection: 'row' }}
+						alignItemsV="center"
 					>
 						<RadioButton
 							style={props.style}
-							selected={option == selection ? true : false}
+							selected={
+								selection && option === selection[0]
+									? true
+									: false
+							}
 							onValueChange={() => {
-								setSelection(option);
+								setSelection([option]);
 								if (props.onChange) props.onChange(option);
 							}}
+							color={props.color}
+							size={props.size}
 						/>
 						<Text style={labelStyle} type="body">
 							{option}
@@ -114,4 +117,4 @@ export function Select(props: Props) {
 	// need alignitems to something other than the default stretch, or the touchable opacity
 	// will expand far past the text and button for an option on the select
 	return <View style={{ alignItems: 'flex-start' }}>{optionElemets}</View>;
-}
+};
