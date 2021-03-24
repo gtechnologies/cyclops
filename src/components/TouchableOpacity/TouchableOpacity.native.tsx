e622ea36349e1a4c2842ee8542aa40e8a406f0e3
Brian Li <brian.li.101@gmail.com>
2021-03-24 11:01:35 -0700

    fixing native TouchableOpacity alignment

--- src/components/TouchableOpacity/TouchableOpacity.native.tsx
+++ src/components/TouchableOpacity/TouchableOpacity.native.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import { TouchableOpacity as RNTouchableOpacity } from 'react-native';
+import {
+	TouchableOpacity as RNTouchableOpacity,
+	ViewStyle,
+} from 'react-native';
 
 import { TouchableOpacityPropsNative as TouchableOpacityProps } from './TouchableOpacityTypes';
 
 import { getOnPressFromProps } from '../../types/Clickable';
 
+import { getAlignStyle } from '../../styles/Alignment';
+
 export const TouchableOpacity = React.forwardRef<
 	RNTouchableOpacity,
 	TouchableOpacityProps
 >((props, ref) => {
+	let alignStyle = getAlignStyle(
+		props.style,
+		props.alignItemsH,
+		props.alignItemsV,
+	);
+
+	let style = {
+		...alignStyle,
+		...(props.style as ViewStyle),
+	};
+
 	return (
 		<RNTouchableOpacity
 			ref={ref}
 			{...props}
+			style={style}
 			{...getOnPressFromProps(props)}
 		>
 			{props.children}
